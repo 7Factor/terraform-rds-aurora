7d243dc46882ee83a23a24689fa4d94c24a0078f
chris-malloy <chrismalloymusic@gmail.com>
2019-07-10 10:02:52 -0400

    Converting to HCL2 expressions.

--- main.tf
+++ main.tf
@@ -4,42 +4,42 @@ terraform {
 
 # Look up the primary VPC
 data "aws_vpc" "primary_vpc" {
-  id = "${var.vpc_id}"
+  id = var.vpc_id
 }
 
 resource "aws_rds_cluster" "aurora_cluster" {
   cluster_identifier  = "${var.db_name}-aurora-cluster"
   deletion_protection = true
-  engine              = "${var.db_engine}"
+  engine              = var.db_engine
 
   final_snapshot_identifier = "${var.db_name}-aurora-final-snapshot"
 
-  db_subnet_group_name = "${aws_db_subnet_group.rds_subnet_group.name}"
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
 
   vpc_security_group_ids = flatten([
-    "${aws_security_group.allow_aurora_access.id}",
-    "${var.additional_db_security_groups}",
+    aws_security_group.allow_aurora_access.id,
+    var.additional_db_security_groups,
   ])
 
-  database_name   = "${var.db_name}"
-  master_username = "${var.db_username}"
-  master_password = "${var.db_password}"
-  port            = "${var.db_port}"
+  database_name   = var.db_name
+  master_username = var.db_username
+  master_password = var.db_password
+  port            = var.db_port
 }
 
 resource "aws_rds_cluster_instance" "aurora_db" {
-  count              = "${var.db_instance_count}"
+  count              = var.db_instance_count
   identifier         = "${var.db_name}-instance"
-  cluster_identifier = "${aws_rds_cluster.aurora_cluster.cluster_identifier}"
+  cluster_identifier = aws_rds_cluster.aurora_cluster.cluster_identifier
 
-  engine              = "${var.db_engine}"
+  engine              = var.db_engine
   publicly_accessible = false
 
   # left blank
   engine_version = ""
 
-  db_subnet_group_name = "${aws_db_subnet_group.rds_subnet_group.name}"
-  instance_class       = "${var.db_instance_class}"
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  instance_class       = var.db_instance_class
 
   # adding this as an extra precaution.
   # an explicit deletion protection does not exist for instances
--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "db_name" {
-  value       = "${aws_rds_cluster.aurora_cluster.database_name}"
+  value       = aws_rds_cluster.aurora_cluster.database_name
   description = "The name of the database."
 }
 
 output "db_master_username" {
-  value       = "${aws_rds_cluster.aurora_cluster.master_username}"
+  value       = aws_rds_cluster.aurora_cluster.master_username
   description = "The master username for the aurora cluster."
 }
 
 output "db_port" {
-  value       = "${aws_rds_cluster.aurora_cluster.port}"
+  value       = aws_rds_cluster.aurora_cluster.port
   description = "The port the database is running on."
 }
