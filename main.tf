4c19ffbcc19711317159fadf0c94a36e84b3d2e7
Jeremy Duvall <jduv@7factor.io>
2020-06-23 11:42:19 -0400

    blanking engine

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_rds_cluster_instance" "aurora_db" {
   publicly_accessible = false
 
   engine_version = var.engine_version
-  engine         = var.engine
+  engine         = ""
 
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
   instance_class       = var.db_instance_class
